bf357c2b4866625fb044191a8c7ce59d73e5a2d4
Ziyan Liu <z363liu@edu.uwaterloo.ca>
2019-03-30 20:28:42 -0400

    cleanup

--- main.py
+++ main.py
@@ -30,23 +30,25 @@ numerical_value = X[:, 1:]
 numerical_value = np.append(numerical_value, yearID, axis=1)
 numerical_value[np.isnan(numerical_value.tolist())] = 0
 
+def generate_graph(tree, idx, criterion):
+    dot = StringIO()
+    export_graphviz(tree,
+                    out_file=dot,
+                    feature_names=feature,
+                    class_names=['Nominated', 'Elected'],
+                    filled=True,
+                    rounded=True,
+                    special_characters=True)
+    tree_snapshot = pydot.graph_from_dot_data(dot_data.getvalue())
+    tree_snapshot[0].write_png(str(idx) + "_" + criterion + "_snapshot.png")
+
 for i in range(5):
     print("--------------------------------------------------")
     X_train, X_test, y_train, y_test = train_test_split(numerical_value, Y, test_size = 0.2)
 
     clf_gini = DecisionTreeClassifier(criterion = "gini")
     clf_gini.fit(X_train, y_train)
-    # dot_data = export_graphviz(clf_gini, out_file=None)
-    # graph = export_graphviz.Source(dot_data)
-    # graph.render("%i_gini" %i)
-
-    dot_data = StringIO()
-    export_graphviz(clf_gini, out_file=dot_data,
-                    filled=True, rounded=True,
-                    special_characters=True)
-    graph = pydot.graph_from_dot_data(dot_data.getvalue())
-    graph[0].write_png("%i_gini.png"%i)
-
+    generate_graph(clf_gini, i, "gini")
 
     y_pred_gini = clf_gini.predict(X_test)
     print ("Gini Accuracy is ", accuracy_score(y_test,y_pred_gini)*100)
@@ -56,19 +58,7 @@ for i in range(5):
 
     clf_entropy = DecisionTreeClassifier(criterion = "entropy")
     clf_entropy.fit(X_train, y_train)
-    # dot_data = export_graphviz(clf_entropy, out_file=None)
-    # graph = export_graphviz.Source(dot_data)
-    #graph.render("%i_entropy" % i)
-    dot_data = StringIO()
-    export_graphviz(clf_entropy,
-                    out_file=dot_data,
-                    feature_names= feature,
-                    class_names = ['nominated','elected'],
-                    filled=True,
-                    rounded=True,
-                    special_characters=True)
-    graph = pydot.graph_from_dot_data(dot_data.getvalue())
-    graph[0].write_png("%i_entropy.png"%i)
+    generate_graph(clf_entropy, i, "entropy")
 
     y_pred_entropy = clf_entropy.predict(X_test)
     print ("Entropy Accuracy is ", accuracy_score(y_test,y_pred_entropy)*100)
